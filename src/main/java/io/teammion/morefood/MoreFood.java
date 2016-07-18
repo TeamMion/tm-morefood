42d9c87fa2897a365569b2a95be04e7771e946c1
Stefan Wimmer <stefanwimmer128@gmail.com>
2016-07-18 22:37:06 +0200

    Fix server side rendering, Release 1.1.0

--- src/main/java/io/teammion/morefood/MoreFood.java
+++ src/main/java/io/teammion/morefood/MoreFood.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 /**
  * Created by Stefan Wimmer <stefanwimmer128@gmail.com> on 05.06.16.
@@ -25,8 +26,11 @@ public class MoreFood
     @Mod.EventHandler
     public void init(FMLInitializationEvent e)
     {
-        Items.render();
-        Blocks.render();
+        if (e.getSide() == Side.CLIENT)
+        {
+            Items.render();
+            Blocks.render();
+        }
         
         ShapedRecipes.register();
         ShapelessRecipes.register();
